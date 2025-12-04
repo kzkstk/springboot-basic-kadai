7f7ebea44f8c7fef05e9b5625c79d3f4b8f254d3
kzkstk <kazuki-10.6.10@ezweb.ne.jp>
2025-12-04 19:29:38 +0900

    Add files via upload

--- spring_kadai_form/src/main/java/form/ContactForm.java
+++ spring_kadai_form/src/main/java/form/ContactForm.java
@@ -1,13 +1,34 @@
 package form;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+/*フォームクラスを定義し、お問い合わせフォームの各入力項目を管理。
+ * バリデーション条件（後述）も設定する。
+ */
 
-@SpringBootApplication
+@Data
 public class ContactForm {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ContactForm.class, args);
-	}
+	@NotBlank(message = "お名前を入力してください")
+	 private String name;//お名前
 
+	    // メールアドレス
+	@NotBlank(message = "メールアドレスを入力してください")
+	@Email(message = "メールアドレスの入力形式が正しくありません")
+	    private String email;
+
+	    // お問い合わせ内容
+	@NotBlank(message = "お問い合わせ内容を確認してください")
+	    private String message;
+	
+	
+	/* public String getName() { return name; }
+	    public void setName(String name) { this.name = name; }
+
+	    public String getEmail() { return email; }
+	    public void setEmail(String email) { this.email = email; }
+
+	    public String getMessage() { return message; }
+	    public void setMessage(String message) { this.message = message; }*/
 }
